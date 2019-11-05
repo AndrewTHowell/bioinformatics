feb66394235520545c4a5b5843895482789228c9
AndrewTHowell <howelldrew99@gmail.com>
2019-11-05 13:29:29 +0000

    Functions initiated

--- bioinformatics.py
+++ bioinformatics.py
@@ -1,1 +1,13 @@
 # Bioinformatics Coursework
+
+# Basic Dynamic Programming
+# Output: [score of best alignment,
+#          sequence1]
+def dynprog(alphabet, scoring_matrix, sequence1, sequence2):
+    pass
+
+def dynproglin(alphabet, scoring_matrix, sequence1, sequence2):
+    pass
+
+def heuralign(alphabet, scoring_matrix, sequence1, sequence2):
+    pass

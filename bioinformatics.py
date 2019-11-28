dae334da5e56b0ee514cb5c959ddec77f3b7cb3d
AndrewTHowell <howelldrew99@gmail.com>
2019-11-28 12:05:44 +0000

    Signatures set up

--- bioinformatics.py
+++ bioinformatics.py
@@ -1,4 +1,5 @@
-# Bioinformatics Coursework
+import numpy as np
+
 
 # Basic Dynamic Programming
 # Output: [score of best alignment,
@@ -6,8 +7,22 @@
 def dynprog(alphabet, scoring_matrix, sequence1, sequence2):
     pass
 
+
 def dynproglin(alphabet, scoring_matrix, sequence1, sequence2):
     pass
 
+
 def heuralign(alphabet, scoring_matrix, sequence1, sequence2):
     pass
+
+
+a = dynprog("ABC",
+            [[1, -1, -2, -1],
+             [-1, 2, -4, -1],
+             [-2, -4, 3, -2],
+             [-1, -1, -2, 0]],
+            "AABBAACA",
+            "CBACCCBA")
+
+print("Score:   ", a[0])
+print("Indices: ", a[1], a[2])

a893a8202d5d39b06e116105a249dbd503a6bbe8
AndrewTHowell <howelldrew99@gmail.com>
2019-11-28 12:33:12 +0000

    regions added

--- bioinformatics.py
+++ bioinformatics.py
@@ -1,28 +1,37 @@
 import numpy as np
 
 
-# Basic Dynamic Programming
-# Output: [score of best alignment,
-#          sequence1]
-def dynprog(alphabet, scoring_matrix, sequence1, sequence2):
+# Region: Quadratic Dynamic Programming
+
+def dynprog(alphabet, scoringMatrix, sequence1, sequence2):
     pass
 
 
-def dynproglin(alphabet, scoring_matrix, sequence1, sequence2):
+# Region End
+
+# Region: Linear Space Dynamic Programming
+
+def dynproglin(alphabet, scoringMatrix, sequence1, sequence2):
     pass
 
 
-def heuralign(alphabet, scoring_matrix, sequence1, sequence2):
+# Region End
+
+# Region: Heuristic Align Dynamic Programming
+
+def heuralign(alphabet, scoringMatrix, sequence1, sequence2):
     pass
 
 
-a = dynprog("ABC",
-            [[1, -1, -2, -1],
-             [-1, 2, -4, -1],
-             [-2, -4, 3, -2],
-             [-1, -1, -2, 0]],
-            "AABBAACA",
-            "CBACCCBA")
+# Region End
 
-print("Score:   ", a[0])
-print("Indices: ", a[1], a[2])
+result = dynprog("ABC",
+                 [[1, -1, -2, -1],
+                  [-1, 2, -4, -1],
+                  [-2, -4, 3, -2],
+                  [-1, -1, -2, 0]],
+                 "AABBAACA",
+                 "CBACCCBA")
+
+print("Score:   ", result[0])
+print("Indices: ", result[1], result[2])
